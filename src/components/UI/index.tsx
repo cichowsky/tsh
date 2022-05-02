5a87053d3b87f54cb32b2bdcd26e6a9995539f89
cichowsky <mrcichowsky@gmail.com>
2022-05-02 19:30:17 +0200

    components: add index file to export UI components

---
+++ src/components/UI/index.tsx
@@ -0,0 +1,8 @@
+import Button from './Button';
+import Checkbox from './Checkbox';
+import FormGroup from './FormGroup';
+import Heading from './Heading';
+import Input from './Input';
+import Spinner from './Spinner';
+
+export { Button, Checkbox, FormGroup, Heading, Input, Spinner };
